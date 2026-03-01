98045f2431029555d1339af2f6023fe9184e4217
marouane-m7b <marouane.ma7boub@gmail.com>
2026-03-01 12:35:00 +0000

    Create an implementation of the IMetier interface using loose coupling.

---
+++ src/net/mahboub/metier/MetierImpl.java
@@ -0,0 +1,37 @@
+package net.mahboub.metier;
+
+import net.mahboub.dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao; // Couplage Faible
+    @Override
+    public double calcul() {
+        double t = dao.getData();
+        double res = t * 12 * Math.random() / Math.PI;
+        return res;
+    }
+
+    /**
+     * Pour injecter dans l'attribut dao un objet d'une classe
+     * qui implemente l'interface IDao au moment de l'instanciation
+     * Plus preferable que setDao
+     */
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    public MetierImpl() {
+    }
+
+    /**
+     * Pour injecter dans l'attribut dao un objet d'une classe
+     * qui implemente l'interface IDao
+     */
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+
+
+}
